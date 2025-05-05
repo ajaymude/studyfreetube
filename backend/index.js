d779270e8f6456911660a1d028f60622b173447f
ajaymude <mudeajay@gmail.com>
2025-05-05 09:59:06 +0530

    setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import path from "path";
+const __dirname = path.resolve();
+const app = express();
+
+// path should not be the same
+app.get("/test", (req, res) => {
+  res.send("test");
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(process.env.PORT, () => {
+  console.log("runnig");
+});
